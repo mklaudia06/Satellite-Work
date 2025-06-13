199ac0ee7256059ca16bf9abe683fee0aeeb783b
mklaudia06 <mklaudiaalfonso@email.com>
2025-06-13 03:06:24 -0400

    story

--- storytelling/tools.py
+++ storytelling/tools.py
@@ -1,4 +1,9 @@
 import streamlit as st 
+import pandas as pd
+import json
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 def for_titles_centered (text, level="h1", color="blue"):
     st.markdown(
@@ -6,4 +11,20 @@ def for_titles_centered (text, level="h1", color="blue"):
         unsafe_allow_html=True
     )
 
+
+def graph_per_year ():
+    route = "./json/total_launches_by_country_and_year.json"
+    with open (route,"r",encoding='utf8') as f:
+        data = json.load(f)
+        years = [i for i in range(1957,2025)]
+        satellites_usa = []
+        satellites_rus = []
+        for i in data['Russia']:
+            satellites_rus.append(i['launches'])
+        for i in data['United_States']:
+            satellites_usa.append(i['launches'])
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x = years,y=satellites_rus,mode="lines+markers", name = "Russian Launches"))
+        fig.add_trace(go.Scatter(x = years,y =satellites_usa,mode= "lines+markers",name="United Stated Launches"))
+    return fig
 
