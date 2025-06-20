9ec8e3a8f05bacba513369c2553cdce74cd481f2
mklaudia06 <mklaudiaalfonso@email.com>
2025-06-20 10:58:11 -0400

    agencies

--- dataproduct/pages/Agency.py
+++ dataproduct/pages/Agency.py
@@ -1,3 +1,39 @@
 import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
 
-st.title("Analisis por agencias")
+st.title("Análisis por agencias")
+#aqui va el texto
+
+ruta = "./json/satelliteucs.json"
+data = pd.read_json(ruta)
+
+country_select = st.selectbox('Elige una opción',options= set(data['owner_country']))
+for _,fila in data.iterrows():
+    country = fila['owner_country']
+    if country == country_select:
+        st.title(f"Number of satellites by agency of {country}")
+        agency_count = {}
+        agencies = fila['owner'] 
+        if agencies in agency_count:
+            agency_count[agencies] += 1
+        else:
+            agency_count[agencies] = 1
+              
+        name_agencias = list(agency_count.keys())
+        cantidad = list(agency_count.values())
+        fig = go.Figure(
+            go.Bar(name =country, x = name_agencias, y=cantidad, marker_color='green')
+            )
+        fig.update_layout(
+            title='',
+            xaxis_title='',
+            yaxis_title='',
+            legend_title=''
+        )
+st.plotly_chart(fig)
+            
+
+        
+
+    
--- streamlit_app/pages/Start.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from src.schemas import Texts
-
-st.html(Texts.dataproduct_header)
-st.html(Texts.satelliteQuiz_header)
-st.html(Texts.start_text)
